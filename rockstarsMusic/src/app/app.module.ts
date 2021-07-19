b49988cecacb95c578399c0c5bd7f2f95436ffb1
Pierik <henk-jan.pierik@rabobank.nl>
2021-07-19 20:07:22 +0200

    Cannot get logo to work either, I suspect the path is not correct or config somewhere. Will start implementing search

--- rockstarsMusic/src/app/app.module.ts
+++ rockstarsMusic/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { ArtistOverviewComponent } from './components/artist-overview/artist-overview.component';
 import {HttpClientModule} from "@angular/common/http";
+import { ArtistSearchComponent } from './components/artist-search/artist-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtistComponent,
-    ArtistOverviewComponent
+    ArtistOverviewComponent,
+    ArtistSearchComponent
   ],
   imports: [
     BrowserModule,
