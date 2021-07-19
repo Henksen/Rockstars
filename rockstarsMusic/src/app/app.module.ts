862a78eeb19ac6d2b13c877ab2afa6b3f73bfaba
Pierik <henk-jan.pierik@rabobank.nl>
2021-07-19 20:32:06 +0200

    Regret not picking component library, but feared the time it would take at the time. Now this kinds of binds will take long

--- rockstarsMusic/src/app/app.module.ts
+++ rockstarsMusic/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ArtistComponent } from './components/artist/artist.component';
 import { ArtistOverviewComponent } from './components/artist-overview/artist-overview.component';
 import {HttpClientModule} from "@angular/common/http";
 import { ArtistSearchComponent } from './components/artist-search/artist-search.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ArtistSearchComponent } from './components/artist-search/artist-search.
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- rockstarsMusic/src/app/components/artist-search/artist-search.component.ts
+++ rockstarsMusic/src/app/components/artist-search/artist-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-artist-search',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-search.component.css']
 })
 export class ArtistSearchComponent implements OnInit {
-
+  @ViewChild('searchInput') searchInput: ElementRef;
+  @ViewChild('submitButton') submitButton: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
